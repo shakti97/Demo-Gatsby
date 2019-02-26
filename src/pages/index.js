c2c40133f62c4d2f7ba82f765c232a2c8910a08d
shakti97 <shaktisingh230797@gmail.com>
2019-02-26 18:08:18 +0530

    Added Layout Components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from "react"
 import About from './about.js';
 import Contact from '../Components/Contact';
+import Layout from '../Components/Layout';
 export default () => 
-            <div>
+            <Layout>
                 <div style={{color : 'red'}}>Hello Gatsby!</div>
                 <p>Makes the development Faster</p>
                 <img src="https://cdn-images-1.medium.com/max/1000/1*G9aVAI3aezHLw_JsiCfB1Q.jpeg" alt="Gatsby"/>
                 <About/>
                 <Contact mobileNumber='987GatsBy98'/>
-            </div>
+            </Layout>
